cc43255bbfb574af3d2014af903cdd59dfa20c9b
Mike Millar <mmillar@unm.edu>
2024-03-16 08:16:45 -0600

    add boilerplate to logreg and gradient scripts

---
+++ gradient.py
@@ -0,0 +1,8 @@
+class GradientDescent:
+    def __init__(self) -> None:
+        # TODO
+        pass
+
+    def fit(self) -> None:
+        # TODO
+        pass
---
+++ logreg.py
@@ -0,0 +1,12 @@
+class LogisticRegression:
+    def __init__(self) -> None:
+        # TODO
+        pass
+
+    def fit(self) -> None:
+        # TODO
+        pass
+
+    def predict(self) -> None:
+        # TODO
+        pass
