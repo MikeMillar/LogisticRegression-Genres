46795dd01bb7d1972b01e1fef2222b49503fa67c
Mike Millar <mmillar@unm.edu>
2024-04-04 17:25:39 -0600

    update music.py to multiple model analysis

--- music.py
+++ music.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import balanced_accuracy_score, recall_score, precision_score
+from sklearn.metrics import balanced_accuracy_score, recall_score, precision_score, f1_score
 from datetime import datetime
 import time
 
@@ -18,19 +18,22 @@ from sklearn.ensemble import GradientBoostingClassifier
 # Internal imports
 import utils
 from logreg import LogisticRegression
+from reportutils import random_search_demo
 
 # Global variables
 # Data variables
-train_path = 'data/train/train_short.csv'        # Path to the training CSV
-test_path = 'data/test/test_short.csv'           # Path to the output CSV
+train_path = 'data/train/music_20_mfcc.csv'        # Path to the training CSV
+test_path = 'data/test/test_20_mfcc.csv'           # Path to the output CSV
 save_path = 'data/result/'
 split_size = 0.2                                # Testing validation split size percent
 variance = 0.85                                 # Percent of variance to keep when reducing dimensionality
+save_output = False                             # Indicates that the output should be saved to file
+reduce = False                                   # Set to True if you want PCA dimensionality reduction
 # Gradient Ascent variables
-learning_rate = 0.01                            # Learning rate used for gradient ascent
-penalty = 1.0                                   # Regularization factor used for gradient ascent
-epsilon = 1e-9                                  # Early termination error difference used for gradient ascent
-max_iterations = int(10e4)                      # Max iterations before termination of gradient ascent
+learning_rate = 0.003705                        # Learning rate used for gradient ascent
+penalty = 0.673836                              # Regularization factor used for gradient ascent
+epsilon = 1e-5                                  # Early termination error difference used for gradient ascent
+max_iterations = 10000                          # Max iterations before termination of gradient ascent
 
 def print_params():
     print(f'train={train_path}, test={test_path}, test_size={split_size}, var={variance}, eta={learning_rate}, pen={penalty}, epsilon={epsilon}, max_iter={max_iterations}')
@@ -101,13 +104,11 @@ def scale_and_reduce(X_train, X_test, Z, variance):
     Z = pd.DataFrame(scaler.transform(Z), columns=Z.columns, index=Z_index)
 
     # Reduce the data dimensionalit using PCA
-    pca = PCA(n_components=variance)
-    # X_train = pca.fit_transform(X_train)
-    # X_test = pca.fit_transform(X_test)
-    # Z = pca.fit_transform(Z)
-    X_train = pd.DataFrame(pca.fit_transform(X_train), index=X_train_index)
-    X_test = pd.DataFrame(pca.transform(X_test), index=X_test_index)
-    Z = pd.DataFrame(pca.transform(Z), index=Z_index)
+    if reduce:
+        pca = PCA(n_components=variance)
+        X_train = pd.DataFrame(pca.fit_transform(X_train), index=X_train_index)
+        X_test = pd.DataFrame(pca.transform(X_test), index=X_test_index)
+        Z = pd.DataFrame(pca.transform(Z), index=Z_index)
     return X_train, X_test, Z
 
 def load_prep_scale(X_path, Z_path, split_size, variance):
@@ -133,13 +134,22 @@ def load_prep_scale(X_path, Z_path, split_size, variance):
     # Load initial data
     X = load_data(X_path)
     Z = load_data(Z_path)
+    Y = X['label']
+    # MFCCs
+    mfcc_cols = []
+    for col in X.columns:
+        if col.startswith('mfcc'):
+            mfcc_cols.append(col)
+    X = X[mfcc_cols]
+    X['label'] = Y
+    Z = Z[mfcc_cols]
     
     # Clean up some data
     X, Y = prepare_data(X, True)
     Z = prepare_data(Z)
 
     # Split the data
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=split_size, stratify=Y)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=split_size, stratify=Y, random_state=None)
 
     # Scale and reduce the data
     X_train, X_test, Z = scale_and_reduce(X_train, X_test, Z, variance)
@@ -166,19 +176,23 @@ def get_model_stats(Y_values, Y_true, Y_pred):
     # Validation Statistics
     balanced_accuracy = balanced_accuracy_score(Y_true, Y_pred)
     adj_balanced_accuracy = balanced_accuracy_score(Y_true, Y_pred, adjusted=True)
-    precision = precision_score(Y_true, Y_pred, average=None, labels=Y_values)
-    recall = recall_score(Y_true, Y_pred, average=None, labels=Y_values)
-    print(f'Model Statistics:\nBalanced Accuracy = {balanced_accuracy}\nAdjusted Balanced Accuracy = {adj_balanced_accuracy}')
-    print('Precision:')
-    for i in range(len(Y_values)):
-        print(f'\t{Y_values[i]}: {precision[i]}')
+    precision = precision_score(Y_true, Y_pred, average='weighted', labels=Y_values, zero_division=0.0)
+    recall = recall_score(Y_true, Y_pred, average='weighted', labels=Y_values)
+    f1 = f1_score(Y_true, Y_pred, average='weighted', labels=Y_values)
+    print(f'\tBalanced Accuracy = {balanced_accuracy}\n\tAdjusted Balanced Accuracy = {adj_balanced_accuracy}')
+    print(f'\tPrecision = {precision}\n\trecall = {recall}\n\tf1 = {f1}')
+    # print('Precision:')
+    # for i in range(len(Y_values)):
+    #     print(f'\t{Y_values[i]}: {precision[i]}')
+    # print('Recall:')
+    # for i in range(len(Y_values)):
+    #     print(f'\t{Y_values[i]}: {recall[i]}')
+    # print('F1 Score:')
+    # for i in range(len(Y_values)):
+    #     print(f'\t{Y_values[i]}: {f1[i]}')
+    return balanced_accuracy, adj_balanced_accuracy, precision, recall, f1
 
-    print('Recall:')
-    for i in range(len(Y_values)):
-        print(f'\t{Y_values[i]}: {recall[i]}')
-    return balanced_accuracy, adj_balanced_accuracy, precision, recall
-
-def train_predict_custom(X_train, X_test, Y_train, Y_test, Z):
+def train_predict_custom(model, X_train, X_test, Y_train, Y_test, Z):
     """
     Trains, tests, and saves the output of the kaggle test file to a csv file.
 
@@ -193,7 +207,6 @@ def train_predict_custom(X_train, X_test, Y_train, Y_test, Z):
         None
     """
     # Create model
-    model = LogisticRegression(learning_rate=learning_rate, penalty=penalty, epsilon=epsilon, max_iterations=max_iterations)
 
     start = time.time()
     # Train the model
@@ -201,11 +214,6 @@ def train_predict_custom(X_train, X_test, Y_train, Y_test, Z):
 
     # Validation testing of the model
     Y_pred = model.predict(X_test)
-
-    # Validation Statistics
-    Y_values = np.unique(Y_train)
-    print(f'Stats for Custom Logistic Regression({time.time() - start} s)')
-    get_model_stats(Y_values, Y_test, Y_pred)
 
     # Run model of actual test data
     out_pred = model.predict(Z)
@@ -217,7 +225,12 @@ def train_predict_custom(X_train, X_test, Y_train, Y_test, Z):
     dt_string = now.strftime("%m-%d-%Y_%H-%M")
     Z['class'].to_csv(save_path + dt_string + '.csv', index_label='id')
 
-def train_predict_other(X_train, X_test, Y_train, Y_test):
+    # Validation Statistics
+    Y_values = np.unique(Y_train)
+    print(f'Stats for Custom Logistic Regression({time.time() - start} s)')
+    return get_model_stats(Y_values, Y_test, Y_pred)
+
+def train_predict_other(model, name, X_train, X_test, Y_train, Y_test):
     """
     Trains and tests on several Sklearn ML models for result comparrison.
 
@@ -233,52 +246,111 @@ def train_predict_other(X_train, X_test, Y_train, Y_test):
     Y_values = np.unique(Y_train)
 
     # Sklearn logistic regression
-    logreg = lr(penalty='l1', max_iter=max_iterations, tol=epsilon, solver='liblinear')
     start = time.time()
-    logreg.fit(X_train, Y_train)
-    lr_pred = logreg.predict(X_test)
-    print(f'Stats for Sklearn Logistic Regression({time.time() - start} s):')
-    get_model_stats(Y_values, Y_test, lr_pred)
+    model.fit(X_train, Y_train)
+    lr_pred = model.predict(X_test)
+    print(f'Stats for {name}({time.time() - start} s):')
+    return get_model_stats(Y_values, Y_test, lr_pred)
 
-    # Sklearn Random Forest
-    rf = RandomForestClassifier(criterion='entropy', max_depth=50)
-    start = time.time()
-    rf.fit(X_train, Y_train)
-    rf_pred = rf.predict(X_test)
-    print(f'Stats for Sklearn Random Forest({time.time() - start} s):')
-    get_model_stats(Y_values, Y_test, rf_pred)
+def run_models(names, models, X_train, X_test, Y_train, Y_test, Z):
+    bal_accs = []
+    adj_bal_accs = []
+    precisions = []
+    recalls = []
+    f1s = []
+    for i in range(len(names)):
+        name = names[i]
+        model = models[i]
+        if name == 'My LR' and save_output:
+            ba, aba, p, r, f = train_predict_custom(model, X_train, X_test, Y_train, Y_test, Z)
+            bal_accs.append(ba)
+            adj_bal_accs.append(aba)
+            precisions.append(p)
+            recalls.append(r)
+            f1s.append(f)
+        else:
+            ba, aba, p, r, f = train_predict_other(model, name, X_train, X_test, Y_train, Y_test)
+            bal_accs.append(ba)
+            adj_bal_accs.append(aba)
+            precisions.append(p)
+            recalls.append(r)
+            f1s.append(f)
+    return bal_accs, adj_bal_accs, precisions, recalls, f1s
 
-    # Sklearn Gaussian Naive Bayes
-    gnb = GaussianNB()
-    start = time.time()
-    gnb.fit(X_train, Y_train)
-    gnb_pred = gnb.predict(X_test)
-    print(f'Stats for Sklearn Gaussian Naive Bayes({time.time() - start} s):')
-    get_model_stats(Y_values, Y_test, gnb_pred)
-
-    # Sklearn Gradient Boosting
-    gbc = GradientBoostingClassifier(max_depth=50)
-    start = time.time()
-    gbc.fit(X_train, Y_train)
-    gbc_pred = gbc.predict(X_test)
-    print(f'Stats for Sklearn Gradient Boosting({time.time() - start} s):')
-    get_model_stats(Y_values, Y_test, gbc_pred)
-
-    # Sklearn SVM
-    svc = svm.SVC()
-    start = time.time()
-    svc.fit(X_train, Y_train)
-    svc_pred = svc.predict(X_test)
-    print(f'Stats for Sklearn SVM({time.time() - start} s):')
-    get_model_stats(Y_values, Y_test, svc_pred)
+# def random_search(X_train, X_test, Y_train, Y_test, iters):
+def random_search(iters):
+    # X_train, X_test, Y_train, Y_test, Z = load_prep_scale(train_path, test_path, split_size, variance)
+    # Randomized hyper parameters
+    etas = []
+    eps = []
+    pens = []
+    # Metrics
+    bals = []
+    adj_bals = []
+    precisions = []
+    recalls = []
+    f1s = []
+    for i in range(iters):
+        X_train, X_test, Y_train, Y_test, Z = load_prep_scale(train_path, test_path, split_size, variance)
+        eta = np.random.uniform(0,0.01)
+        pen = np.random.uniform(0,1)
+        ep = np.random.uniform(1e-15, 0.001)
+        model = LogisticRegression(learning_rate=eta, penalty=pen, epsilon=ep)
+        model.fit(X_train, Y_train)
+        Y_pred = model.predict(X_test)
+        metrics = get_model_stats(np.unique(Y_train), Y_test, Y_pred)
+        etas.append(eta)
+        eps.append(ep)
+        pens.append(pen)
+        bals.append(metrics[0])
+        adj_bals.append(metrics[1])
+        precisions.append(metrics[2])
+        recalls.append(metrics[3])
+        f1s.append(metrics[4])
+    return {
+        'learning_rate': etas,
+        'tolerance': eps,
+        'penalty': pens,
+        'bal_acc': bals,
+        'adj_bal_acc': adj_bals,
+        'precision': precisions,
+        'recall': recalls,
+        'f1': f1s
+    }
 
 if __name__ == "__main__":
+    # results = random_search(10000)
+    # df = pd.DataFrame(results)
+    # random_search_demo(df)
+    # exit()
+    
     print_params()
     # Load and prepare the data
-    X_train, X_test, Y_train, Y_test, Z = load_prep_scale(train_path, test_path, split_size, variance)
+    # X_train, X_test, Y_train, Y_test, Z = load_prep_scale(train_path, test_path, split_size, variance)
 
-    # Test our model
-    train_predict_custom(X_train, X_test, Y_train, Y_test, Z)
+    names = [
+        'My_LR', 
+        'SK_LR', 
+        'SK_RF', 
+        'SK_GNB', 
+        'SK_GB', 
+        'SK_SVM'
+    ]
+    models = [
+        LogisticRegression(learning_rate=learning_rate, penalty=penalty, epsilon=epsilon, max_iterations=max_iterations),
+        lr(penalty='l1', solver='liblinear'),
+        RandomForestClassifier(criterion='entropy', max_depth=50),
+        GaussianNB(),
+        GradientBoostingClassifier(max_depth=50),
+        svm.SVC()
+    ]
 
-    # Test with other models
-    train_predict_other(X_train, X_test, Y_train, Y_test)
+    score_filename = 'data/result/_scores2.csv'
+    for _ in range(100):
+        X_train, X_test, Y_train, Y_test, Z = load_prep_scale(train_path, test_path, split_size, variance)
+        metrics = run_models(names, models, X_train, X_test, Y_train, Y_test, Z)
+        score_file = open(score_filename, 'a')
+        for i in range(len(names)):
+            line = f'{names[i]},{metrics[0][i]},{metrics[1][i]},{metrics[2][i]},{metrics[3][i]},{metrics[4][i]}\n'
+            score_file.write(line)
+        score_file.close()
